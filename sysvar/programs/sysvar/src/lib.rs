ff0ac579aafbebe1ed3330f51af8084eba356062
nazariyv <hi@naz.ooo>
2024-06-29 23:23:05 +0100

    day 11 clock get

--- sysvar/programs/sysvar/src/lib.rs
+++ sysvar/programs/sysvar/src/lib.rs
@@ -6,8 +6,9 @@ declare_id!("HGuq6WzaMQ4DaHyK148JonXBHv74Utw26c6itXZK6Tgs");
 pub mod sysvar {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        let block_timestamp = Clock::get()?;
+        msg!("Current block timestamp: {:?}", block_timestamp);
         Ok(())
     }
 }
